26699d3bee3ae367c86aadcb95dc61cd084c037f
Hothead <home@vovan.ru>
2025-04-19 22:46:08 +0300

    Better regex for benchmark

--- 2/compile_benchmark/benchmark.cpp
+++ 2/compile_benchmark/benchmark.cpp
@@ -3,30 +3,16 @@
 #include "myre.hpp"
 
 int main(){
-	std::string regex = 
-	"aaa*aaaa{2,3}aaaa|aaa{2,3}a|a*aa*aa{2,3}aa|a{2,3}aaa|"
-	"bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa{2,3}a|a*aa*"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"{2,3}a|a*aa*aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbb"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"{2,3}a|a*aa*aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbb"
-	"{2,3}a|a*aa*aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbb"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa"
-	"aa{2,3}aa|a{2,3}aaa|bb{2,3}bbb{2,3}b{2,3}bbbaaa*aaaa{2,3}aaaa|aaa";
+	std::string regex;
+	for (int i=0; i<3000; ++i){
+		regex += 'a';
+		if (i%2 == 0){
+			regex += "{2,3}";
+		}
+		if (i%3 == 0){
+			regex += "|";
+		}
+	}
 	double sum=0;
 	for (int i=0; i<10; ++i){
 		auto start = std::chrono::high_resolution_clock::now();
@@ -36,6 +22,6 @@ int main(){
 		sum += duration.count();
 	}
 	std::cout << "Average time is " << sum/10 << "\n";
-	// 10 ms average
+	// 167 ms average
 	return 0;
 }
